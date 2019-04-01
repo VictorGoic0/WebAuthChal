193b5d6794dcb556112c58ebf072b07e9f923c67
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-01 17:52:57 -0400

    Helper methods for users created

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use("/api/users", userRouter);
+server.use("/api/register", userRouter);
 
 module.exports = server;
