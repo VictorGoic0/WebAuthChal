5703029755e2b3d8dbe0c2604729d0828541137f
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-01 17:56:57 -0400

    Routers for get and post added to register router (get is just for my viewing purposes)

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,13 +1,46 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const db = require("../data/helpers/user-model.js");
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const users = await db.find();
+    if (users) {
+      res.status(200).json(users);
+    }
+  } catch (error) {
+    res.status(500).json({ message: `Users could not be found ${error}.` });
+  }
+});
 
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await db.findById(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res
+        .status(404)
+        .json({ message: "User with specified ID does not exist." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: `User request failed ${error}.` });
+  }
+});
 
-router.post("/", async (req, res) => {});
-
-router.delete("/:id", async (req, res) => {});
-
-router.put("/:id", async (req, res) => {});
+router.post("/", async (req, res) => {
+  const user = req.body;
+  try {
+    const newUser = await db.create(user);
+    if (newUser) {
+      res.status(201).json(newUser);
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Your user could not be usered ${error}.` });
+  }
+});
 
 module.exports = router;
