5bb0b11a9163564e27f2d0b022436b09a6432914
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-01 17:42:40 -0400

    Table for users added to migration file. Router file created

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
-const cors = require("cors");
+const userRouter = require("./routers/userRouter.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+server.use("/api/users", userRouter);
 
 module.exports = server;
